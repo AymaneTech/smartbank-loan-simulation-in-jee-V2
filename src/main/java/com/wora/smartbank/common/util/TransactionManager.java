ff4e5c671577ada50541f88ce1968aa821889739
geek <elmainiaymane03@gmail.com>
2024-10-06 22:16:54 +0100

    create transaction manager helper
    contain two methods one for queries that return result and other for who
    don't retun result

---
+++ src/main/java/com/wora/smartbank/common/util/TransactionManager.java
@@ -0,0 +1,45 @@
+package com.wora.smartbank.common.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceException;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class TransactionManager {
+
+    public static <T> T executeWithResult(EntityManagerFactory emf, Function<EntityManager, T> executor) {
+        try (final EntityManager em = emf.createEntityManager()) {
+            final EntityTransaction tx = em.getTransaction();
+            try {
+                tx.begin();
+                T result = executor.apply(em);
+                tx.commit();
+                return result;
+            } catch (Exception ew) {
+                tx.rollback();
+                throw new PersistenceException("erorr in persistence");
+            } finally {
+                em.close();
+            }
+        }
+    }
+
+    public static void executeWithoutResult(EntityManagerFactory emf, Consumer<EntityManager> executor) {
+        try (final EntityManager em = emf.createEntityManager()) {
+            final EntityTransaction tx = em.getTransaction();
+            try {
+                tx.begin();
+                executor.accept(em);
+                tx.commit();
+            } catch (Exception e) {
+                tx.rollback();
+                throw new PersistenceException("error in persistence");
+            } finally {
+                em.close();
+            }
+        }
+    }
+}
