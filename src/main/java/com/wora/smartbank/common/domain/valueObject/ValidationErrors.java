89a3f3bbbe3ebbfcd6033931ede59abaa1193702
geek <elmainiaymane03@gmail.com>
2024-10-07 03:14:16 +0100

    update validation errors record: add new constructor

--- src/main/java/com/wora/smartbank/common/domain/valueObject/ValidationErrors.java
+++ src/main/java/com/wora/smartbank/common/domain/valueObject/ValidationErrors.java
@@ -1,6 +1,21 @@
 package com.wora.smartbank.common.domain.valueObject;
 
-import java.util.Map;
+import jakarta.validation.ConstraintViolation;
 
-public record ValidationErrors(String message, Map<String, String> errors) {
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record ValidationErrors<T>(String message, Map<String, String> errors) {
+
+    public ValidationErrors(String message, Set<ConstraintViolation<T>> constraintViolations) {
+        this(
+                message,
+                constraintViolations.stream()
+                        .collect(Collectors.toMap(
+                                c -> c.getPropertyPath().toString(),
+                                ConstraintViolation::getMessage
+                        ))
+        );
+    }
 }
