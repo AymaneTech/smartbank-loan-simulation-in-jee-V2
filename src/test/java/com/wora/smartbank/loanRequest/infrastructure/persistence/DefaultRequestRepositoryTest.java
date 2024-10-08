7e7d78983846ea4f0e4de8bbd17bf20f548c7a5b
geek <elmainiaymane03@gmail.com>
2024-10-08 21:48:43 +0100

    do the same as service test in repository test

--- src/test/java/com/wora/smartbank/loanRequest/infrastructure/persistence/DefaultRequestRepositoryTest.java
+++ src/test/java/com/wora/smartbank/loanRequest/infrastructure/persistence/DefaultRequestRepositoryTest.java
@@ -10,10 +10,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.PersistenceException;
 import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -46,119 +43,139 @@ class DefaultRequestRepositoryTest {
         }
     }
 
-    @Test
-    @DisplayName("find all - should return all requests")
-    void findAll_ShouldReturnAllRequests() {
-        Request r1 = RequestSeeder.getRequest();
-        Request r2 = RequestSeeder.getRequest();
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            em.persist(r1);
-            em.persist(r2);
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
+    @Nested
+    @DisplayName("findAll() method tests")
+    class FindAllTests {
+        @Test
+        @DisplayName("Should return all requests when exists")
+        void findAll_ShouldReturnAllRequests() {
+            Request r1 = RequestSeeder.getRequest();
+            Request r2 = RequestSeeder.getRequest();
+            EntityTransaction tx = em.getTransaction();
+            try {
+                tx.begin();
+                em.persist(r1);
+                em.persist(r2);
+                tx.commit();
+            } catch (Exception e) {
+                if (tx != null && tx.isActive()) {
+                    tx.rollback();
+                }
+                throw e;
             }
-            throw e;
+
+            em.clear();
+
+            List<Request> actual = repository.findAll();
+
+            assertTrue(actual.stream().anyMatch(r -> r.id().equals(r1.id())));
+            assertTrue(actual.stream().anyMatch(r -> r.id().equals(r2.id())));
+        }
+    }
+
+    @Nested
+    @DisplayName("findById() method tests")
+    class FindByIdTests {
+        @Test
+        @DisplayName("Should return Optional<Request> when exists")
+        public void findById_GivenValidId_ShouldReturnRequest() {
+            Request request = RequestSeeder.getRequest();
+            Request expected = repository.save(request);
+
+            Optional<Request> actual = repository.findById(expected.id());
+
+            assertEquals(actual.get().id(), expected.id());
         }
 
-        em.clear();
+        @Test
+        @DisplayName("Should return empty optional when given invalid id")
+        public void findById_GivenInvalidId_ShouldReturnEmptyOptional() {
+            RequestId id = new RequestId();
+            Optional<Request> result = repository.findById(id);
 
-        List<Request> actual = repository.findAll();
-
-        assertTrue(actual.stream().anyMatch(r -> r.id().equals(r1.id())));
-        assertTrue(actual.stream().anyMatch(r -> r.id().equals(r2.id())));
+            assertTrue(result.isEmpty());
+        }
     }
 
-    @Test
-    @DisplayName("findById - given valid Id - return request")
-    public void findById_GivenValidId_ShouldReturnRequest() {
-        Request request = RequestSeeder.getRequest();
-        Request expected = repository.save(request);
+    @Nested
+    @DisplayName("save() method tests")
+    class CreateTests {
+        @Test
+        @DisplayName("Should return created request when given valid data")
+        public void create_GivenValidRequest_ShouldReturnCreatedRequest() {
+            Request expected = RequestSeeder.getRequest();
 
-        Optional<Request> actual = repository.findById(expected.id());
+            Request actual = repository.save(expected);
 
-        assertEquals(actual.get().id(), expected.id());
-    }
-
-    @Test
-    @DisplayName("findById - invalid id - should throw requestNotFoundException")
-    public void findById_GivenInvalidId_ShouldReturnEmptyOptional() {
-        RequestId id = new RequestId();
-        Optional<Request> result = repository.findById(id);
-
-        assertTrue(result.isEmpty());
-    }
-
-    @Test
-    @DisplayName("create - valid data - should return created request")
-    public void create_GivenValidRequest_ShouldReturnCreatedRequest() {
-        Request expected = RequestSeeder.getRequest();
-
-        Request actual = repository.save(expected);
-
-        assertEquals(expected.customerDetails(), actual.customerDetails());
-        assertEquals(expected.loanDetails(), actual.loanDetails());
-    }
-
-    @Test
-    @DisplayName("update - given valid request - should return updated request")
-    public void update_GivenValidRequest_ShouldReturnUpdatedRequest() {
-        Request request = RequestSeeder.getRequest();
-        Request saved = repository.save(request);
-        final String purpose = "updated purpose";
-        final String firstName = "ayamne udpate";
-        final String lastName = "el maini update";
-
-        Request expected = RequestSeeder.getRequest().setId(saved.id());
-
-        Request actual = repository.save(expected);
-
-        assertEquals(saved.id(), actual.id());
-    }
-
-    @Test
-    @DisplayName("existsById - existing id - should return true")
-    public void existsById_GivenExistingId_ShouldReturnTrue() {
-        Request expected = RequestSeeder.getRequest();
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            em.persist(expected);
-            tx.commit();
-        } catch (Exception e) {
-            if (tx != null && tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
+            assertEquals(expected.customerDetails(), actual.customerDetails());
+            assertEquals(expected.loanDetails(), actual.loanDetails());
         }
 
-        boolean actual = repository.existsById(expected.id());
+        @Test
+        @DisplayName("Should return updated request when given valid data")
+        public void update_GivenValidRequest_ShouldReturnUpdatedRequest() {
+            Request request = RequestSeeder.getRequest();
+            Request saved = repository.save(request);
+            final String purpose = "updated purpose";
+            final String firstName = "ayamne udpate";
+            final String lastName = "el maini update";
 
-        assertTrue(actual);
+            Request expected = RequestSeeder.getRequest().setId(saved.id());
+
+            Request actual = repository.save(expected);
+
+            assertEquals(saved.id(), actual.id());
+        }
     }
 
-    @Test
-    @DisplayName("delete - given valid request id - should delete")
-    public void delete_GivenValidRequest_ShouldDeleteRequest() {
-        Request saved = repository.save(RequestSeeder.getRequest());
+    @Nested
+    @DisplayName("existsById() method tests")
+    class ExistsByIdTests {
+        @Test
+        @DisplayName("Should return true when give existing id")
+        public void existsById_GivenExistingId_ShouldReturnTrue() {
+            Request expected = RequestSeeder.getRequest();
+            EntityTransaction tx = em.getTransaction();
+            try {
+                tx.begin();
+                em.persist(expected);
+                tx.commit();
+            } catch (Exception e) {
+                if (tx != null && tx.isActive()) {
+                    tx.rollback();
+                }
+                throw e;
+            }
 
-        repository.delete(saved.id());
+            boolean actual = repository.existsById(expected.id());
 
-        assertFalse(repository.existsById(saved.id()));
+            assertTrue(actual);
+        }
+
+        @Test
+        @DisplayName("Should return false when given invalid id")
+        public void existsById_GivenUnexistingId_ShouldReturnFalse() {
+            assertFalse(repository.existsById(new RequestId()));
+        }
     }
 
-    @Test
-    @DisplayName("delete - given invalid request id - should throw request not found exception")
-    public void delete_GivenInvalidRequestId_ShouldThrowRequestNotFoundException() {
-        assertThrows(PersistenceException.class, () -> repository.delete(new RequestId()));
-    }
+    @Nested
+    @DisplayName("delete() method tests")
+    class DeleteTests {
+        @Test
+        @DisplayName("Should delete request when given id exists")
+        public void delete_GivenValidRequest_ShouldDeleteRequest() {
+            Request saved = repository.save(RequestSeeder.getRequest());
 
-    @Test
-    @DisplayName("existsById - not existing id - should return false")
-    public void existsById_GivenUnexistingId_ShouldReturnFalse() {
-        assertFalse(repository.existsById(new RequestId()));
+            repository.delete(saved.id());
+
+            assertFalse(repository.existsById(saved.id()));
+        }
+
+        @Test
+        @DisplayName("Should delete throw request not found exception when given invalid id")
+        public void delete_GivenInvalidRequestId_ShouldThrowRequestNotFoundException() {
+            assertThrows(PersistenceException.class, () -> repository.delete(new RequestId()));
+        }
     }
 }
