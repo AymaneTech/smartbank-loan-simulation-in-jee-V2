a23b597c76b24680a9d0d6fbfba29e6a83a328c1
geek <elmainiaymane03@gmail.com>
2024-10-10 13:07:28 +0100

    add id class to constructor of default jpa repository

--- src/main/java/com/wora/smartbank/common/producers/JpaRepositoryProducer.java
+++ src/main/java/com/wora/smartbank/common/producers/JpaRepositoryProducer.java
@@ -2,6 +2,7 @@ package com.wora.smartbank.common.producers;
 
 import com.wora.smartbank.orm.api.JpaRepository;
 import com.wora.smartbank.orm.internal.DefaultJpaRepository;
+import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 import jakarta.enterprise.inject.spi.InjectionPoint;
 
@@ -10,11 +11,13 @@ import java.lang.reflect.ParameterizedType;
 public class JpaRepositoryProducer {
 
     @Produces
+    @Dependent
     @SuppressWarnings("unchecked")
     public <T, ID> JpaRepository<T, ID> createJpaRepository(InjectionPoint injectionPoint) {
         ParameterizedType type = (ParameterizedType) injectionPoint.getType();
         Class<T> entityClass = (Class<T>) type.getActualTypeArguments()[0];
-        return new DefaultJpaRepository<T, ID>() {
-        };
+        Class<ID> idClass = (Class<ID>) type.getActualTypeArguments()[1];
+
+        return new DefaultJpaRepository<T, ID>(entityClass, idClass);
     }
 }
