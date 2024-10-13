dc07cc4cc45ddd042f4d9786777f5f0eb942225d
geek <elmainiaymane03@gmail.com>
2024-10-13 15:08:48 +0100

    update the status request dto, add description to dto

--- src/main/java/com/wora/smartbank/loanRequest/application/dto/RequestStatusRequest.java
+++ src/main/java/com/wora/smartbank/loanRequest/application/dto/RequestStatusRequest.java
@@ -1,11 +1,12 @@
 package com.wora.smartbank.loanRequest.application.dto;
 
-import com.wora.smartbank.loanRequest.domain.valueObject.RequestId;
-import com.wora.smartbank.loanRequest.domain.valueObject.StatusId;
+import com.wora.smartbank.loanRequest.domain.Request;
+import com.wora.smartbank.loanRequest.domain.entity.Status;
 import jakarta.validation.constraints.NotNull;
 
 public record RequestStatusRequest(
-        @NotNull StatusId statusId,
-        @NotNull RequestId requestId
+        @NotNull String description,
+        @NotNull Status status,
+        @NotNull Request request
 ) {
 }
