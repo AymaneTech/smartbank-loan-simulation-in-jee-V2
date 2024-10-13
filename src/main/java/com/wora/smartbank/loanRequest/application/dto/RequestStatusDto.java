7505d9a51a576be8ddd1ec3a1ac211681fba0849
geek <elmainiaymane03@gmail.com>
2024-10-13 20:17:03 +0100

    chnage request status dto

---
+++ src/main/java/com/wora/smartbank/loanRequest/application/dto/RequestStatusDto.java
@@ -0,0 +1,12 @@
+package com.wora.smartbank.loanRequest.application.dto;
+
+import com.wora.smartbank.loanRequest.domain.valueObject.RequestId;
+import com.wora.smartbank.loanRequest.domain.valueObject.StatusId;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestStatusDto(
+        @NotNull String description,
+        @NotNull StatusId statusId,
+        @NotNull RequestId requestId
+) {
+}
