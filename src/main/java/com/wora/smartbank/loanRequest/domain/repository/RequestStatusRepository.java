cd916086f81600e5b6b69d7a6867d5e95620e9a3
geek <elmainiaymane03@gmail.com>
2024-10-13 15:10:38 +0100

    create reqeust status repository

---
+++ src/main/java/com/wora/smartbank/loanRequest/domain/repository/RequestStatusRepository.java
@@ -0,0 +1,8 @@
+package com.wora.smartbank.loanRequest.domain.repository;
+
+import com.wora.smartbank.loanRequest.domain.entity.RequestStatus;
+import com.wora.smartbank.loanRequest.domain.valueObject.RequestStatusId;
+import com.wora.smartbank.orm.api.JpaRepository;
+
+public interface RequestStatusRepository extends JpaRepository<RequestStatus, RequestStatusId> {
+}
